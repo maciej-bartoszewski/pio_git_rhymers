c426f94a01a1a4af06111563d6e4d37ddbc75ccf
Maciej Bartoszewski <maciejbartoszewski02@gmail.com>
2024-05-29 13:06:00 +0200

    17. Napisanie dokumentacji dla DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa jest implementacja prostego stosu
+ */
 public class DefaultCountingOutRhymer {
 
     private static final int ARRAY_SIZE = 12;
@@ -11,25 +14,45 @@ public class DefaultCountingOutRhymer {
     private int total = EMPTY;
 
 
+    /**
+     * Dodaje liczbe do tablicy
+     * @param in liczba do dodania
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * Sprawdza czy lista jest pusta
+     * @return true - jesli lista jest pusta, badz false - jesli nie jest
+     */
     public boolean callCheck() {
         return total == EMPTY;
     }
 
+    /**
+     * Sprawdza czy lista jest pełna
+     * @return true - jesli lista jest pelna, badz false - jesli nie jest
+     */
     public boolean isFull() {
         return total == CAPACITY;
     }
 
+    /**
+     * Sprawdza ostatnio dodana liczbe
+     * @return IF_EMPTY (-1) - jesli lista jest pusta, badz ostatnio dodana liczbe
+     */
     protected int peekaboo() {
         if (callCheck())
             return IF_EMPTY;
         return numbers[total];
     }
 
+    /**
+     * Usuwa ostatnio dodana liczbe z listy
+     * @return IF_EMPTY (-1) - jesli lista jest pusta, badz ostatnio dodana liczbe
+     */
     public int countOut() {
         if (callCheck())
             return IF_EMPTY;
