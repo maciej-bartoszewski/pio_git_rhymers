37f4ed58e270d65ee3eb2a43ac6cd22617a0fd3e
Maciej Bartoszewski <maciejbartoszewski02@gmail.com>
2024-05-29 12:27:12 +0200

    15. Walidacja projektu testami

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RhymersJUnitTest {
-
+    // Wszystkie testy zakończone pozytywanie
     @Test
     public void testCountIn() {
         DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
